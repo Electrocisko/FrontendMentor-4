2efd5f4d624b8203853ef43f771b1b94e449be98
Francisco Zuidwijk <electrocisko@gmail.com>
2023-11-07 14:35:25 -0300

    DOM Partial

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const submitBtn = document.querySelector('.submit-card');
 const pointersBTns = document.querySelectorAll('.point-button');
+const buttonsContainer = document.querySelector('.point-buttons-container');
+const iconContainer = document.querySelector('.icon-container');
+const titleH2 = document.querySelector('h2');
+const paragraph = document.querySelector('p');
+const illustrationContainer = document.querySelector('.illustration-container');
+const cardContainer = document.querySelector('.card-container');
+const selected = document.querySelector('.selected');
+
+
+
 
 function handlerSubmit(e) {
-    console.log(e.target);
+    buttonsContainer.classList.add('hidden');
+    submitBtn.classList.add('hidden');
+    iconContainer.classList.add('hidden');
+    titleH2.innerText = 'Thank you!';
+    paragraph.innerText= `  We appreciate you taking the time to give a rating. If you ever need more support, 
+    don’t hesitate to get in touch!`;
+    illustrationContainer.classList.remove('hidden');
+    cardContainer.classList.add('text-center');
+    selected.classList.remove('hidden');
+ 
 }
 
 function handlerPoint(e) {
