c21f58d769b021b739ba025afdbf5b3202e6dcdc
Francisco Zuidwijk <electrocisko@gmail.com>
2023-11-07 13:54:34 -0300

    addeventlisteners in buttons

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Front end mentor challenge');
+const submitBtn = document.querySelector('.submit-card');
+const pointersBTns = document.querySelectorAll('.point-button');
+
+function handlerSubmit(e) {
+    console.log(e.target);
+}
+
+function handlerPoint(e) {
+    console.log(e.target.value);
+}
+
+for (let i = 0; i < pointersBTns.length; i++) {
+pointersBTns[i].addEventListener('click', (e) => handlerPoint(e))
+}
+
+submitBtn.addEventListener('click', (e) => handlerSubmit(e));
+
